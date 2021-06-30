5bda24eec1131349581e03c4b307ee2367926e73
viveshs <viveshs@discovery.co.za>
2021-06-30 21:05:19 +0200

    first commit

---
+++ src/main/java/com/bmw/test/stack/Stack.java
@@ -0,0 +1,71 @@
+package com.bmw.test.stack;
+import java.util.Arrays;
+
+/**
+ * Stack implementation
+ * 
+ *
+ */
+public class Stack<T> {
+	
+	private static final int DEFAULT_LENGTH  = 10;
+	
+	private int topIndex = -1; //holds the index of recently pushed element
+	
+	private Object[] data; //actual data
+	
+	public Stack() {
+		this.data = new Object[DEFAULT_LENGTH]; //initial size of 10;
+	}
+	
+	/**
+	 * Adds element to the array. increase array size if it exceeds current limit.
+	 * Synchronzed - to make it thread safe.
+	 * 
+	 * @param e
+	 */
+	public synchronized void push(T e) { 
+		if(this.topIndex >= this.data.length-1) { //if the array full, grow the array size
+			this.data = Arrays.copyOf(this.data, this.data.length + DEFAULT_LENGTH);
+		}
+		this.data[++topIndex] = e; //incrment topIndex and add the element at that position.
+	}
+	
+	/**
+	 * Remove top element
+	 * Synchronized - to make it thread safe.
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public synchronized T pop() {
+		
+		if(this.isEmpty()) throw new IllegalStateException("Can't pop as the stack is empty.");
+		
+		T popped = (T) this.data[this.topIndex]; //get the most recently pushed
+		this.data[topIndex--] = null; //remove the elment at the top index and decrment topIndxe
+		
+		System.out.println("pop -> " + popped);
+		
+		return popped;
+	}
+	
+	/**
+	 * if top is negative, there are no elemts.
+	 * 
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return this.topIndex < 0;
+	}
+	
+	/**
+	 * returns the size of the stack.
+	 * 
+	 * @return
+	 */
+	public int count() {
+		return this.topIndex + 1;
+	}
+
+}
---
+++ src/test/java/com/bmw/test/stack/StackTest.java
@@ -0,0 +1,53 @@
+package com.bmw.test.stack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StackTest {
+	
+	@Test
+	public void whenPushed() {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(42);
+		stack.push(66);
+		stack.push(99);
+		Assert.assertEquals(3, stack.count());
+		Assert.assertFalse(stack.isEmpty());
+	}
+
+	@Test
+	public void whenPopped() {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(42);
+		stack.push(66);
+		stack.push(99);
+		Assert.assertEquals(99, stack.pop().intValue());
+		Assert.assertEquals(66, stack.pop().intValue());
+		Assert.assertEquals(42, stack.pop().intValue());
+	}
+	
+	@Test
+	public void whenPushedMorethanArrayInitialSizeItMustResize() {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(42);
+		stack.push(66);
+		stack.push(99);
+		stack.push(33);
+		stack.push(44);
+		stack.push(78);
+		stack.push(57);
+		stack.push(98);
+		stack.push(25);
+		stack.push(84);
+		stack.push(26);
+		stack.push(24);
+		Assert.assertEquals(12, stack.count()); //more than 10 elements
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void whenNoElemtsaAndPoppedItMustThrowError() {
+		Stack<Integer> stack = new Stack<>();
+		Assert.assertEquals(null, stack.pop());
+	}
+	
+}
